2cb767e9c1b893bce6ccb98afc23ca42ddd17d87
Luis Ng Tang <lngtang@stevens.edu>
2023-09-27 19:04:10 -0400

    homepage and navbar draft

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,22 @@
-export default function navbar (){
-    return(
-        <h1>This is the navbar</h1>
-    );
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function navbar() {
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark" sticky='top'>
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
+
+export default navbar;
