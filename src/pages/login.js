184daceaf10c0d148b54f481ab28a5cbe3160ed0
jaiminbd13 <98934222+jaiminbd13@users.noreply.github.com>
2023-10-29 12:49:17 -0400

    Update login.js

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,25 +1,22 @@
-import Navbar from "../components/navbar.js";
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.css";
+import { Container, Form } from "react-bootstrap";
 import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import { Container } from "react-bootstrap";
-import '../css/login.css'
-import React, { useState } from 'react';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
+import Navbar from "../components/navbar.js";
+import '../css/login.css';
 
 const Login = () => {
-
-    const [logInfo, setlogInfo] = useState({
+    const [logInfo, setLogInfo] = useState({
         email_address: '',
         password: ''
     });
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
-        setlogInfo({
+        setLogInfo({
             ...logInfo,
             [name]: value
         });
@@ -27,20 +24,26 @@ const Login = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-    
+
         const { email_address, password } = logInfo;
-    
-        // Check if both fields are filled
+
         if (!email_address || !password) {
             toast.error('Both fields are required.', {
                 position: toast.POSITION.TOP_RIGHT
             });
-            return;
+        } else {
+            // You should make an API request for authentication here
+            // and handle success or failure accordingly
+            // For now, we'll just log the form data
+            console.log('Form submitted:', logInfo);
+
+            // If authentication is successful, you can redirect to another page
+            // For example, you can use React Router for this.
+            // history.push('/dashboard'); // Import history from react-router-dom
         }
-        console.log('Form submitted:', logInfo);
     };
 
-    return(
+    return (
         <div>
             <Navbar />
             <ToastContainer />
@@ -50,42 +53,38 @@ const Login = () => {
                     <Form.Group className="mb-3" controlId="formEmail">
                         <Form.Label>Email address</Form.Label>
                         <Form.Control
-                        type="email"
-                        placeholder="Email address"
-                        id="email_address"
-                        name="email_address" 
-                        value={logInfo.email_address}
-                        onChange={handleInputChange}
+                            type="email"
+                            placeholder="Email address"
+                            id="email_address"
+                            name="email_address"
+                            value={logInfo.email_address}
+                            onChange={handleInputChange}
                         />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formPassword">
                         <Form.Label>Password</Form.Label>
                         <Form.Control
-                        type="password"
-                        placeholder="Password"
-                        id="password"
-                        name="password" 
-                        value={logInfo.password}
-                        onChange={handleInputChange}
+                            type="password"
+                            placeholder="Password"
+                            id="password"
+                            name="password"
+                            value={logInfo.password}
+                            onChange={handleInputChange}
                         />
                     </Form.Group>
                     <Form.Group className="d-grid gap-2">
-                    <Button className="mb-5" type="submit" variant="success" size="lg">
-                        Log in
-                    </Button>
-
-                    {/* Added an "Admin Login" button */}
-                    <Link to="/admin-login">
-                        <Button variant="primary" size="lg">
-                            Admin Login
+                        <Button className="mb-5" type="submit" variant="success" size="lg">
+                            Log in
                         </Button>
-                    </Link>
-                        
+                        <Link to="/admin-login">
+                            <Button variant="primary" size="lg">
+                                Admin Login
+                            </Button>
+                        </Link>
                     </Form.Group>
                 </Form>
             </Container>
         </div>
-        
     );
 };
 
