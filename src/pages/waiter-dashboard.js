140cd3b8a17c9e660fedcd0d80ea75d7b49589a0
Ameya172 <123523953+Ameya172@users.noreply.github.com>
2023-10-31 10:33:28 +0300

    Create waiter-dashboard.js

---
+++ src/pages/waiter-dashboard.js
@@ -0,0 +1,13 @@
+import Navbar from "../components/navbar.js";
+
+
+const WaiterDashBoard = () => {
+    return(
+        <div>
+            <Navbar />
+            <h1>Waiter Dashboard</h1>
+        </div>
+    );
+}  
+
+export default WaiterDashBoard;
