4e3a9d08b5de63e07b7e62d026b867a1a2be97f8
Ameya172 <123523953+Ameya172@users.noreply.github.com>
2023-10-31 10:32:06 +0300

    Create user-dashboard.js

---
+++ src/pages/user-dashboard.js
@@ -0,0 +1,13 @@
+import Navbar from "../components/navbar.js";
+
+
+const UserDashBoard = () => {
+    return(
+        <div>
+            <Navbar />
+            <h1>User Dashboard</h1>
+        </div>
+    );
+}  
+
+export default UserDashBoard;
