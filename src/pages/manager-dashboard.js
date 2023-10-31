0ad89de1b526ce69591df6f5e6975dc3f4c191b0
Ameya172 <123523953+Ameya172@users.noreply.github.com>
2023-10-31 10:28:32 +0300

    Create manager-dashboard.js

---
+++ src/pages/manager-dashboard.js
@@ -0,0 +1,13 @@
+import Navbar from "../components/navbar.js";
+
+
+const ManagerDashBoard = () => {
+    return(
+        <div>
+            <Navbar />
+            <h1>Manager Dashboard</h1>
+        </div>
+    );
+}  
+
+export default ManagerDashBoard;
