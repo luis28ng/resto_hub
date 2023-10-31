1a775847ff41b90127070a7e192b051093fe5725
Ameya172 <123523953+Ameya172@users.noreply.github.com>
2023-10-31 10:31:18 +0300

    Create staff-dashboard.js

---
+++ src/pages/staff-dashboard.js
@@ -0,0 +1,13 @@
+import Navbar from "../components/navbar.js";
+
+
+const StaffDashBoard = () => {
+    return(
+        <div>
+            <Navbar />
+            <h1>Staff Dashboard</h1>
+        </div>
+    );
+}  
+
+export default StaffDashBoard;
