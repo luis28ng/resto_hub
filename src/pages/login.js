c9c28418657460a13d8c0e0207b412a95e2f625f
Ameya172 <123523953+Ameya172@users.noreply.github.com>
2023-10-31 10:24:48 +0300

    Update login.js

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,49 +1,87 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import "bootstrap/dist/css/bootstrap.css";
-import { Container, Form } from "react-bootstrap";
-import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router-dom';
+
+import axios from 'axios'
+import { Container } from "react-bootstrap";
 import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 import Navbar from "../components/navbar.js";
-import '../css/login.css';
+
+import "bootstrap/dist/css/bootstrap.css";
+import 'react-toastify/dist/ReactToastify.css';
+
+import '../css/login.css'
+
+
 
 const Login = () => {
-    const [logInfo, setLogInfo] = useState({
+
+    const [logInfo, setlogInfo] = useState({
         email_address: '',
         password: ''
     });
 
+    const navigate =useNavigate();
+
     const handleInputChange = (e) => {
         const { name, value } = e.target;
-        setLogInfo({
+        setlogInfo({
             ...logInfo,
             [name]: value
         });
     };
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-
+    
         const { email_address, password } = logInfo;
-
+    
+        // Check if both fields are filled
         if (!email_address || !password) {
             toast.error('Both fields are required.', {
                 position: toast.POSITION.TOP_RIGHT
             });
-        } else {
-            // You should make an API request for authentication here
-            // and handle success or failure accordingly
-            // For now, we'll just log the form data
-            console.log('Form submitted:', logInfo);
+            return;
+        }
 
-            // If authentication is successful, you can redirect to another page
-            // For example, you can use React Router for this.
-            // history.push('/dashboard'); // Import history from react-router-dom
+        try{
+            // Make a POST request to the backend for authentication
+            const response = await axios.post(
+                `http://localhost:8080/login?email=${email_address}&password=${password}`
+            );
+
+            const jwtToken = response.data.jwtToken;
+            const userRole = response.data.userRole;
+
+            // Store the JWT token and user role in LocalStorage
+            localStorage.setItem('jwtToken', jwtToken);
+            localStorage.setItem('userRole', userRole);
+
+            // Redirect the user based on their role 
+            if (userRole === 'RESTOHUB_OWNER') {
+                navigate.push('/admin-dashboard');
+            }if (userRole === 'RESTAURANT_MANAGER') {
+                navigate.push('/manager-dashboard');
+            }if (userRole === 'RESTAURANT_STAFF') {
+                navigate.push('/staff-dashboard');
+            }if (userRole === 'RESTAURANT_WAITER') {
+                navigate.push('/waiter-dashboard');
+            } else{
+                navigate.push('/user-dashboard');
+            }
+        } catch (e) { 
+            // Handle authentication errors
+            console.log(e)
+            toast.error('Authentication failed. Please Check your credentials', {
+                position: toast.POSITION.TOP_CENTER
+            });
         }
     };
 
-    return (
+    return(
         <div>
             <Navbar />
             <ToastContainer />
@@ -53,39 +91,37 @@ const Login = () => {
                     <Form.Group className="mb-3" controlId="formEmail">
                         <Form.Label>Email address</Form.Label>
                         <Form.Control
-                            type="email"
-                            placeholder="Email address"
-                            id="email_address"
-                            name="email_address"
-                            value={logInfo.email_address}
-                            onChange={handleInputChange}
+                        type="email"
+                        placeholder="Email address"
+                        id="email_address"
+                        name="email_address" 
+                        value={logInfo.email_address}
+                        onChange={handleInputChange}
                         />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formPassword">
                         <Form.Label>Password</Form.Label>
                         <Form.Control
-                            type="password"
-                            placeholder="Password"
-                            id="password"
-                            name="password"
-                            value={logInfo.password}
-                            onChange={handleInputChange}
+                        type="password"
+                        placeholder="Password"
+                        id="password"
+                        name="password" 
+                        value={logInfo.password}
+                        onChange={handleInputChange}
                         />
                     </Form.Group>
                     <Form.Group className="d-grid gap-2">
-                        <Button className="mb-5" type="submit" variant="success" size="lg">
-                            Log in
-                        </Button>
-                        <Link to="/admin-login">
-                            <Button variant="primary" size="lg">
-                                Admin Login
-                            </Button>
-                        </Link>
+                    <Button className="mb-5" type="submit" variant="success" size="lg">
+                        Log in
+                    </Button>
                     </Form.Group>
                 </Form>
             </Container>
         </div>
     );
+        
+        
+    
 };
 
 export default Login;
