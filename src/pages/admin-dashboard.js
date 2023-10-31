a2463cb59e9243cb35d2572f9a7705570ef7cc8f
Ameya172 <123523953+Ameya172@users.noreply.github.com>
2023-10-31 10:23:59 +0300

    Create admin-dashboard.js

---
+++ src/pages/admin-dashboard.js
@@ -0,0 +1,13 @@
+import Navbar from "../components/navbar.js";
+
+
+const AdminDashBoard = () => {
+    return(
+        <div>
+            <Navbar />
+            <h1>Admin Dashboad</h1>
+        </div>
+    );
+}  
+
+export default AdminDashBoard;
