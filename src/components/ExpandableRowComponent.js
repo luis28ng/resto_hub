11c72700b3d35e3c089835e84cb9f148f02c141e
Ameya172 <123523953+Ameya172@users.noreply.github.com>
2023-11-26 18:32:54 +0300

    Create ExpandableRowComponent.js

---
+++ src/components/ExpandableRowComponent.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import OrderForm from './forms/OrderForm';
+
+const ExpandableRowComponent = ({ data }) => {
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const handleRowClick = () => {
+        setIsExpanded(true);
+    };
+
+    return (
+        <Container>
+            {/* Render your regular row content here */}
+            <Row
+                style={{ cursor: 'pointer', borderBottom: '1px solid #ddd', padding: '10px' }}
+                onClick={handleRowClick}
+            >
+                {/* Render the OrderForm component with necessary props */}
+                <OrderForm customer={data} restaurantId={data.restaurantId} />
+            </Row>
+        </Container>
+    );
+};
+
+export default ExpandableRowComponent;
