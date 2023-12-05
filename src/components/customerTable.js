7123f5f43289b0d91e847c1a8a879c329704117a
Ameya172 <123523953+Ameya172@users.noreply.github.com>
2023-12-05 06:22:55 +0300

    Update customerTable.js, Debugging

--- src/components/customerTable.js
+++ src/components/customerTable.js
@@ -1,54 +1,28 @@
-// CustomerTable.js
-import React, { useState } from 'react';
+// CheckedInCustomersTable.js
+import React from 'react';
+import DataTable from 'react-data-table-component';
+import ExpandableRowComponent from '../ExpandableRowComponent';
 
-import { Button, Modal, Table } from 'react-bootstrap';
+const CheckedInCustomersTable = ({ checkedInCustomers }) => {
+    const columns = [
+        { name: 'First Name', selector: (row) => row.firstName, center: true, sortable: true },
+        { name: 'Last Name', selector: (row) => row.lastName, center: true, sortable: true },
+        { name: 'Email Address', selector: (row) => row.emailAddress, center: true, sortable: true },
+        { name: 'Party Size', selector: (row) => row.partySize, center: true, sortable: true },
+        { name: 'Reservation Date', selector: (row) => row.reservationDate, center: true, sortable: true },
 
-import OrderForm from './OrderForm';
-
-const CustomerTable = ({ customers }) => {
-    const [selectedCustomer, setSelectedCustomer] = useState(null);
-    const [showOrderModal, setShowOrderModal] = useState(false);
-
-    const handleSelectCustomer = (customer) => {
-        setSelectedCustomer(customer);
-        setShowOrderModal(true);
-    };
+    ];
 
     return (
-        <div>
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Table Number</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {customers.map((customer) => (
-                        <tr key={customer.id}>
-                            <td>{customer.name}</td>
-                            <td>{customer.tableNumber}</td>
-                            <td>
-                                <Button onClick={() => handleSelectCustomer(customer)}>View Orders</Button>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </Table>
-
-            {selectedCustomer && (
-                <Modal show={showOrderModal} onHide={() => setShowOrderModal(false)}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>{`Food Orders for ${selectedCustomer.name}`}</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <OrderForm customer={selectedCustomer} />
-                    </Modal.Body>
-                </Modal>
-            )}
-        </div>
+        <DataTable
+            columns={columns}
+            data={checkedInCustomers}
+            fixedHeader
+            striped
+            expandableRows
+            expandableRowsComponent={ExpandableRowComponent}
+        />
     );
 };
 
-export default CustomerTable;
+export default CheckedInCustomersTable;
