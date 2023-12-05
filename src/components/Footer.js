c4301a5cd0fef68fa05620961125948e330131df
Ameya172 <123523953+Ameya172@users.noreply.github.com>
2023-12-05 06:36:24 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
 import { Navbar, Container, NavbarBrand } from "react-bootstrap";
 
 const Footer = () => {
-    return(
-        <div className="fixed-bottom">  
+    return (
+        <div className="fixed-bottom">
             <Navbar color="dark" bg="dark" variant="dark" sticky="bottom">
                 <Container>
                     <NavbarBrand>
