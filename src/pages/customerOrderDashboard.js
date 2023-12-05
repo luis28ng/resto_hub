26c75331dd4eb969515a37e93440be14ef1c0da3
Ameya172 <123523953+Ameya172@users.noreply.github.com>
2023-12-05 06:40:38 +0300

    Update customerOrderDashboard.js

--- src/pages/customerOrderDashboard.js
+++ src/pages/customerOrderDashboard.js
@@ -1,14 +1,73 @@
-import React from 'react';
-import StripeCheckoutForm from '../components/forms/StripeCheckoutForm';
+import React, { useEffect, useState } from 'react';
+import { Container } from 'react-bootstrap';
+import axios from 'axios';
+import Navbar from "../components/navbar.js";
+import { getRestId } from "../utils/utils.js";
+import AddOrderForm from '../components/forms/AddOrderForm.js';
+import CustomersTable from '../components/tables/CustomerTable.js';
 
-//TODO: Dynamically pass amount to StripeCheckoutForm
+const apiUrl = 'http://restohub-api.us-east-2.elasticbeanstalk.com';
+
 const WaiterDashBoard = () => {
-  return (
-    <div>
-      <h1>Customer Order Dashboard</h1>
-      <StripeCheckoutForm amount={1000} /> 
-    </div>
-  );
+    const [checkedInCustomers, setCheckedInCustomers] = useState([]);
+    const [showAddOrderForm, setShowAddOrderForm] = useState(false);
+    const [restaurantId, setRestaurantId] = useState("");
+
+    useEffect(() => {
+        setRestaurantId(getRestId());
+        fetchCheckedInCustomers();
+    }, []);
+
+    const fetchCheckedInCustomers = async () => {
+        try {
+            const response = await axios.get(`${apiUrl}/api/reservations/checkedIn`, {
+                params: {
+                    restaurantId: getRestId()
+                }
+            });
+            console.log(response);
+            setCheckedInCustomers(response.data);
+        } catch (error) {
+            console.error('Error fetching checked-in customers:', error);
+            setCheckedInCustomers([]);
+        }
+    };
+
+    const handleCloseAddOrderForm = () => {
+        setShowAddOrderForm(false);
+    };
+
+    return (
+        <div>
+            <Navbar />
+            <Container>
+                <br></br>
+                <h1>Customer order dashboard</h1>
+                {checkedInCustomers.length === 0 ? (
+                    <div>
+                        <p>No checked-in customers yet</p>
+                    </div>
+                ) : (
+                    <div>
+                        <Container>
+                            <CustomersTable checkedInCustomers={checkedInCustomers} />
+                        </Container>
+                    </div>
+                )}
+                {showAddOrderForm && (
+                    <AddOrderForm
+                        handleClose={handleCloseAddOrderForm}
+                        restaurantId={restaurantId}
+                        handleAddOrder={(orderRequest) => {
+                            // Replace the following log with your actual API call to submit the order
+                            console.log('Order Submitted:', orderRequest);
+                            handleCloseAddOrderForm();
+                        }}
+                    />
+                )}
+            </Container>
+        </div>
+    );
 };
 
 export default WaiterDashBoard;
